6b2632a878e09a1ec8250d41d056451145952ac2
Дмитрий Богданов <bogdanov@it-limon.ru>
2021-12-13 19:35:45 +0500

    Небольшие правки после ревью

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -6,7 +6,7 @@ export default function SearchForm() {
       <form className='form search-form' name='search-form' onSubmit={() => {}}>
         <fieldset className='search-form__container'>
           <div className='search-form__loupe' />
-          <input className='search-form__input' name='movie' type='text' placeholder='Фильм' />
+          <input className='search-form__input' name='movie' type='text' placeholder='Фильм' required />
           <button className='button form__button-submit' type='submit'>Найти</button>
         </fieldset>
         <label className='search-form__switcher-label'>
